cf2077e5ee57001ce57020fcbb07a71b6cc92980
nhestrompia <nhestrompia@gmail.com>
2026-02-03 21:33:05 +0200

    feat: landing analytics

--- apps/main/main/app/layout.tsx
+++ apps/main/main/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
